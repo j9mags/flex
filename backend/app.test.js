ed0dd911cc5aaea9af747dfc5da6c55a5956ca44
jan magayanes <janine.magayanes@code.berlin>
2021-12-02 20:42:39 +0100

    added order route, model and test

--- backend/app.test.js
+++ backend/app.test.js
@@ -119,3 +119,128 @@ describe("Delete a product", () => {
     });
   });
 });
+
+describe("Get all orders", () => {
+  describe("Given the orders", () => {
+    test("should respond with a 200 status code", async () => {
+      const response = await request(app).post("/api/users/signin").send({
+        email: "jas@gmail.com",
+        password: "password",
+        isAdmin: true,
+        name: "jas",
+      });
+      request(app).get("/api/orders");
+      expect(response.statusCode).toBe(200);
+    });
+  });
+});
+
+describe("Get all orders", () => {
+  describe("Given the orders", () => {
+    test("should respond with a 200 status code", async () => {
+      const response = await request(app).post("/api/users/signin").send({
+        email: "jas@gmail.com",
+        password: "password",
+        isAdmin: true,
+        name: "jas",
+      });
+      request(app).get("/api/orders/mine");
+      expect(response.statusCode).toBe(200);
+    });
+  });
+});
+
+describe("Add an order", () => {
+  describe("given the orders", () => {
+    test("should respond with a 200 status code", async () => {
+      const response = await request(app).post("/api/users/signin").send({
+        email: "js@gmail.com",
+        password: "password",
+      });
+      request(app)
+        .post("/api/orders/")
+        .send({
+          orderItems: {
+            name: "jas",
+            price: 5,
+            image: "test",
+            brand: "wacker",
+            category: "accessory",
+            countInStock: 3,
+            description: "test",
+          },
+          user: 123,
+          shipping: {
+            address: "test",
+            city: "test",
+            postalCode: "test",
+            country: "test",
+          },
+          payment: "paypal",
+          itemsPrice: 30,
+          taxPrice: 10,
+          shippingPrice: 10,
+          totalPrice: 40,
+        });
+      expect(response.statusCode).toBe(200);
+    });
+  });
+});
+
+describe("Get one order", () => {
+  describe("Given the orders", () => {
+    test("should respond with a 200 status code", async () => {
+      const response = await request(app).post("/api/users/signin").send({
+        email: "jas@gmail.com",
+        password: "password",
+        isAdmin: true,
+        name: "jas",
+      });
+      request(app).get("/api/orders/1234");
+      expect(response.statusCode).toBe(200);
+    });
+  });
+});
+
+describe("Delete an order", () => {
+  describe("given the orders", () => {
+    test("should respond with a 200 status code", async () => {
+      const response = await request(app).post("/api/users/signin").send({
+        email: "jas@gmail.com",
+        password: "password",
+        isAdmin: true,
+        name: "jas",
+      });
+      request(app).delete("/api/orders/1234/");
+      expect(response.statusCode).toBe(200);
+    });
+  });
+});
+
+describe("Pay one order", () => {
+  describe("Given the orders", () => {
+    test("should respond with a 200 status code", async () => {
+      const response = await request(app).post("/api/users/signin").send({
+        email: "jas@gmail.com",
+        password: "password",
+        isAdmin: true,
+        name: "jas",
+      });
+      request(app)
+        .put("/api/orders/1234")
+        .send({
+          isPaid: true,
+          paidAt: Date.now(),
+          payment: {
+            paymentMethod: "paypal",
+            paymentResult: {
+              payerID: 12,
+              orderID: 1234,
+              paymentID: 12345,
+            },
+          },
+        });
+      expect(response.statusCode).toBe(200);
+    });
+  });
+});
--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
+app.use("/api/orders", orderRoute);
 
 app.listen(config.PORT, () => {
   console.log("Server started at http://localhost:5000");
